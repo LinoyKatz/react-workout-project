97ebd82d2a384f81621c9c8b1c1a8486e53eaf45
Linoy Katz <katzlinoy1269@gmail.com>
2023-03-31 13:13:27 +0300

    imges

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import REACTLOGO from '/FullStack2022/React/REACTprojects/trainapp/src/media/logoNoBgc.png'
+import React from "react";
+// import REACTLOGO from '/FullStack2022/React/REACTprojects/trainapp/src/media/logoNoBgc.png'
+// import REACTLOGO from "../../../public/media/logoNoBgc.png";
 
-import './appFooter.css'
+import "./appFooter.css";
 import { SlSocialInstagram } from "react-icons/sl";
 import { SlSocialLinkedin } from "react-icons/sl";
 import { BsWhatsapp } from "react-icons/bs";
@@ -9,18 +10,31 @@ import { SlSocialGithub } from "react-icons/sl";
 
 const AppFooter = () => {
   return (
-    <div className='footer'>
-      <section className='string-section'>
-      <ul>
-        <li><SlSocialInstagram color='white'/></li>
-        <li><SlSocialLinkedin color='white'/></li>
-        <li><BsWhatsapp color='white'/></li>
-        <li><SlSocialGithub color='white'/></li>
-      </ul>
+    <div className="footer">
+      <section className="string-section">
+        <ul>
+          <li>
+            <SlSocialInstagram color="white" />
+          </li>
+          <li>
+            <SlSocialLinkedin color="white" />
+          </li>
+          <li>
+            <BsWhatsapp color="white" />
+          </li>
+          <li>
+            <SlSocialGithub color="white" />
+          </li>
+        </ul>
       </section>
-      <section className='logo-section'><img src={REACTLOGO} alt="" /></section>
+      <section className="logo-section">
+        <img
+          src="https://res.cloudinary.com/arikxl/image/upload/v1680257494/Ella2023/ktkvfqst0wucbijnpcub.png"
+          alt="linoy"
+        />
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default AppFooter
+export default AppFooter;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,61 +1,86 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom'
-import WelcomeSectionButton from '../../components/WelcomeSection/WelcomeSectionButton'
-import './homePage.css'
+import { Link } from "react-router-dom";
+import WelcomeSectionButton from "../../components/WelcomeSection/WelcomeSectionButton";
+import "./homePage.css";
 import { FiCheckCircle } from "react-icons/fi";
-import TRXGIRL from '../../media/trx.png.png'
-import WORKINGOUT from '../../media/workingOut.png.png'
-
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className='welcome-h1'>Welcome to our studio</h1>
+      <h1 className="welcome-h1">Welcome to our studio</h1>
 
-      <div className='welcome-section'>
-      {/* <article>
+      <div className="welcome-section">
+        {/* <article>
         <h2>Great to have you back here...</h2>
         <hr />
      </article> */}
 
-      
-      <section className='about-section'>
-        <span> Hello! </span>
-        <br />
-        <p> Our workout planner app is a mobile application designed to help users plan, track, and achieve their fitness goals! </p><br />
-        <Link to="/training">
-        <WelcomeSectionButton/>
-      </Link>
-         
-          <section className='first-about-section'>
-             <img className='grl-img' src={TRXGIRL} alt="trx-girl" />
-           <br /> <br />
-         <ul className='fst-ul'>
-           <li>On our app you can easily</li>
-          <li><FiCheckCircle color='green' size='20px'/> Register/remove yourself from classes in our studio</li>
-          <li><FiCheckCircle color='green' size='20px'/> filter your workouts by your chosen coach/exercise</li>
-          <li><FiCheckCircle color='green' size='20px'/> Easily track your progress by seeing your previous and future workouts</li>
-</ul>
-  
-         <br />
-        
-         </section>
-         <section className='second-about-section'>
-        <img className='working-out-img' src={WORKINGOUT} alt="working-out-guys" />
-          <br /><br />
+        <section className="about-section">
+          <span> Hello! </span>
+          <br />
+          <p>
+            {" "}
+            Our workout planner app is a mobile application designed to help
+            users plan, track, and achieve their fitness goals!{" "}
+          </p>
+          <br />
+          <Link to="/training">
+            <WelcomeSectionButton />
+          </Link>
 
-<ul className='scnd-ul'>
-  <li>We also have</li>
-  <li><FiCheckCircle color='green' size='20px'/> workout videos and Explanations</li>
-  <li><FiCheckCircle color='green' size='20px'/> edit option for your own workout plan</li>
-  <li><FiCheckCircle color='green' size='20px'/> workout videos</li>
-</ul>
-</section>
-      </section>
+          <section className="first-about-section">
+            <img
+              className="grl-img"
+              src="https://res.cloudinary.com/arikxl/image/upload/v1680257248/Ella2023/gx31w77kn9i8buzzlbnh.png"
+              alt="trx-girl"
+            />
+            <br /> <br />
+            <ul className="fst-ul">
+              <li>On our app you can easily</li>
+              <li>
+                <FiCheckCircle color="green" size="20px" /> Register/remove
+                yourself from classes in our studio
+              </li>
+              <li>
+                <FiCheckCircle color="green" size="20px" /> filter your workouts
+                by your chosen coach/exercise
+              </li>
+              <li>
+                <FiCheckCircle color="green" size="20px" /> Easily track your
+                progress by seeing your previous and future workouts
+              </li>
+            </ul>
+            <br />
+          </section>
+          <section className="second-about-section">
+            <img
+              className="working-out-img"
+              src="https://res.cloudinary.com/arikxl/image/upload/v1680257315/Ella2023/s6aetmbqho2awa1uujos.png"
+              alt="working-out-guys"
+            />
+            <br />
+            <br />
+
+            <ul className="scnd-ul">
+              <li>We also have</li>
+              <li>
+                <FiCheckCircle color="green" size="20px" /> workout videos and
+                Explanations
+              </li>
+              <li>
+                <FiCheckCircle color="green" size="20px" /> edit option for your
+                own workout plan
+              </li>
+              <li>
+                <FiCheckCircle color="green" size="20px" /> workout videos
+              </li>
+            </ul>
+          </section>
+        </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
